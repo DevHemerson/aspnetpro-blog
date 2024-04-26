aa3ee67e7d95623235a47500e3f1eabc722b4930
LAPTOP-696GJNV6\hemer <hemersondamasceno90@gmail.com>
2024-04-26 18:53:38 -0300

    Montar a tela com post do banco de dados

---
+++ AspNetPro.Blog/Models/ViewModels/PostItemListViewModel.cs
@@ -0,0 +1,9 @@
+﻿namespace AspNetPro.Blog.Models.ViewModels;
+
+public record PostItemListViewModel
+{
+    public int Id { get; set; }
+    public string? Title { get; set; }
+    public string? Summary { get; set; }
+    public string? PublishedOn { get; set; }
+}
--- AspNetPro.Blog/Pages/Index.cshtml.cs
+++ AspNetPro.Blog/Pages/Index.cshtml.cs
@@ -1,12 +1,26 @@
+using AspNetPro.Blog.Infrastructure.Data;
+using AspNetPro.Blog.Models.entities;
+using AspNetPro.Blog.Models.ViewModels;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
 namespace AspNetPro.Blog.Pages
 {
-    public class IndexModel : PageModel
+    public class IndexModel(BlogContext blogContext) : PageModel
     {
+        public IList<PostItemListViewModel> Posts { get; set; }
         public void OnGet()
         {
+            this.Posts =  blogContext.Posts
+                .OrderByDescending(p => p.PublishedOn)
+                .Select(x => new PostItemListViewModel
+                {
+                    Id = x.Id,
+                    Title = x.Title,
+                    Summary = x.Summary,
+                    PublishedOn = x.PublishedOn.Value.ToShortDateString(),
+                })
+                .ToList();
         }
     }
 }
