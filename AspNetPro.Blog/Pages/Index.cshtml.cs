267d3aea9a96d4860bcbfe458d26d8d4f7a7043c
LAPTOP-696GJNV6\hemer <hemersondamasceno90@gmail.com>
2024-04-26 16:49:46 -0300

    Customizar template para a lista de posts

---
+++ AspNetPro.Blog/Pages/Index.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace AspNetPro.Blog.Pages
+{
+    public class IndexModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
